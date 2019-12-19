d0468ec2ca545314e74bf9acef9c242b53a5bb9f
MatviyO <33325458+MatviyO@users.noreply.github.com>
2019-12-19 17:33:42 +0200

    add1Test

---
+++ react.js
@@ -0,0 +1,22 @@
+//
+// function Car(props) {
+//     return (
+//         <div className="card">
+//             <div className="card-img">
+//                 <img src={props.car.img} alt={props.car.name} />
+//             </div>
+//             <h3>{props.car.name}</h3>
+//             <p>{props.car.price}</p>
+//         </div>
+//     )
+// }
+// const app = (
+//     <div className="app">
+//         <div className="list">
+//             <Car car={{name: 'Bmw M2 coupe', img: 'https://c4d709dd302a2586107d-f8305d22c3db1fdd6f8607b49e47a10c.ssl.cf1.rackcdn.com/thumbnails/stock-images/068dcbe9a6dedf0fb07b907dd96aa4ad.png', price: 18900}}></Car>
+//         </div>
+//     </div>
+// )
+
+// ReactDOM.render(app, document.getElementById('root'))
+
