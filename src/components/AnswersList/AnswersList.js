508f18ef9ed186e68ab92e1459f12640c185caea
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-03-04 12:15:37 +0200

    addp7

---
+++ src/components/AnswersList/AnswersList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './AnswersList.css'
+import AnswerItem from "./AnswerItem/AnswerItem";
+const AnswersList = props => {
+    return (<ul className={classes.AnswersList}>
+        {props.answers.map((answer, index) => {
+            return (
+                <AnswerItem
+                    answer={answer}
+                    key={index}
+                    onAnswerClick={props.onAnswerClick}
+                />
+            )
+        })}
+    </ul> )
+}
+export default AnswersList
+
--- src/containers/quiz/Quiz.js
+++ src/containers/quiz/Quiz.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react'
 import classes from './Quiz.css'
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component {
 
     state = {
-        quiz: []
+        quiz: [
+            {
+                question: 'What a model bmw?',
+                rightAnswerId: 3,
+                answers: [
+                    {text: 'A4', id: 1},
+                    {text: 'rav4', id: 2},
+                    {text: '3 series', id: 3},
+                    {text: 'santa fe', id: 4} ]}
+        ]
+    }
+    onAnswerClickHandler = (answerId) => {
+        console.log(answerId)
     }
     render() {
         return (
             <div className={classes.Quiz}>
-                <h1>Quiz</h1>
+                <div className={classes.QuizWrapper}>
+                    <h1>Quiz</h1>
+                    <ActiveQuiz
+                        answers={this.state.quiz[0].answers}
+                    question={this.state.quiz[0].question}
+                        onAnswerClick={this.onAnswerClickHandler}
+                    />
+                </div>
             </div>
         )
     }
