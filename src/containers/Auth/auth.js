51633bd280428ef7411f0f0dabb3257389f1a8c9
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-03-10 18:54:48 +0200

    addform7

--- src/containers/Auth/auth.js
+++ src/containers/Auth/auth.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react'
 import classes from './auth.css'
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+import is from 'is_js';
+
 
 export default class Auth extends Component {
 
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                 value: '',
@@ -40,21 +43,52 @@ export default class Auth extends Component {
 
     }
     submitHandler = (event) => {
-event.preventDefault()
+        event.preventDefault()
     }
+
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+        let isValid = true
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (validation.email) {
+            isValid = is.email(value) && isValid
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+
+    }
+
     onChangeHandler = (event, controlName) => {
 
         const formControls = {...this.state.formControls}
-        const contro = {...formControls[controlName]}
-        this.setState({
+        const control = {...formControls[controlName]}
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+        formControls[controlName] = control
 
+        let isFormValid = true
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({
+            formControls, isFormValid
         })
     }
 
     renderInputs() {
-          return Object.keys(this.state.formControls).map((controlName, index) => {
-                const control = this.state.formControls[controlName]
-              return (
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
                 <Input
                     key={controlName + index}
                     type={control.type}
@@ -75,14 +109,14 @@ event.preventDefault()
     render() {
         return (
             <div className={classes.Auth}>
-                    <div>
-                        <h1>Authentification</h1>
-                        <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                            {this.renderInputs()}
-                            <Button type="success" onClick={this.loginHandler}>Login</Button>
-                            <Button type="primary" onClick={this.registerHandler} >Registration</Button>
-                        </form>
-                    </div>
+                <div>
+                    <h1>Authentification</h1>
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        {this.renderInputs()}
+                        <Button type="success" disabled={!this.state.isFormValid} onClick={this.loginHandler}>Login</Button>
+                        <Button type="primary" disabled={!this.state.isFormValid} onClick={this.registerHandler}>Registration</Button>
+                    </form>
+                </div>
             </div>
         )
     }
--- src/containers/QuizzCreator/QuizzCreator.js
+++ src/containers/QuizzCreator/QuizzCreator.js
@@ -1,10 +1,86 @@
 import React, {Component} from 'react';
+import classes from './QuizzCreator.css'
+import Button from "../../components/UI/Button/Button";
+import {createControl} from "../../form/formFramework";
+import Input from "../../components/UI/Input/Input";
+function createOptionControl(number) {
+    return createControl({
+        label: `Change ${number}`,
+        errorMessage: 'Not a null',
+        id: number
+    }, {required: true})
+}
+function createFormControls() {
+    return {
+        question: createControl({
+            label: 'Input question',
+            errorMessage: 'Question not a null'
+        }, {required: true}),
+            option1: createOptionControl(1),
+        option2: createOptionControl(2),
+        option3: createOptionControl(3),
+        option4: createOptionControl(4),
+
+    }
+}
 
 export default class QuizCreator extends Component {
+    state = {
+        quiz: [],
+        formControls: createFormControls()
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+    }
+    addQuestionHandler = () => {
+
+    }
+    createQuizHandler = () => {
+
+    }
+    changeHandler = (value, controlName) => {
+
+    }
+
+    renderControls() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                label={control.label}
+                value={control.value}
+                valid={control.valid}
+                shouldValidate={!!control.validation}
+                touched={control.touched}
+                errorMessage={control.errorMessage}
+                onChange={event => this.changeHandler(event.target.value, controlName)}
+
+                />
+            { index === 0 ? <hr /> : null }
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>QuizCreator</h1>
+            <div className={classes.QuizzCreator}>
+                <div>
+                    <h1>Create Test</h1>
+                    <form onSubmit={this.submitHandler} >
+                        {this.renderControls()}
+                        <select name="" id=""></select>
+                        <Button
+                        type="primary"
+                        onClick={this.addQuestionHandler}
+                        >Add question</Button>
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}
+                        >Create test</Button>
+                    </form>
+                </div>
             </div>
         )
     }
